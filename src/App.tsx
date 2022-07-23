6f79df39328a8d5cd495af056ae971cda2274f55
kyamashiro <kyamashiro73@gmail.com>
2022-07-23 23:34:09 +0900

    custom-event

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface MyObject {
+  id: number
 }
 
-export default App;
+function App () {
+  const [id, setId] = useState(0)
+
+  useEffect(() => {
+    const handleEvent = ((event: CustomEvent<MyObject>) => {
+      console.log(event)
+      setId(event.detail.id)
+    }) as EventListenerOrEventListenerObject
+
+    document.addEventListener('custom-event', handleEvent)
+
+    return () => {
+      document.removeEventListener('custom-event', handleEvent)
+    }
+  })
+
+  const onClick = () => {
+    console.log('onClick')
+    const obj: MyObject = { id: id + 1 }
+    document.dispatchEvent(new CustomEvent('custom-event', { detail: obj }))
+  }
+
+  return (
+      <div className="App">
+        <p>{id}</p>
+        <button onClick={onClick}>Event Dispatch</button>
+      </div>
+  )
+}
+
+export default App
